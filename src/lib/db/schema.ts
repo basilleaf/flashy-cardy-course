9ea9c14715f014f58d8858edb4ea8061a99317d2
Lisa Ballard <lballard.cat@gmail.com>
2026-04-03 15:54:08 -0700

    setup neon postgres

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,21 @@
+import { integer, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const decks = pgTable("decks", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  clerkUserId: varchar({ length: 255 }).notNull(),
+  title: varchar({ length: 255 }).notNull(),
+  description: text(),
+  createdAt: timestamp().notNull().defaultNow(),
+  updatedAt: timestamp().notNull().defaultNow(),
+});
+
+export const cards = pgTable("cards", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  deckId: integer()
+    .notNull()
+    .references(() => decks.id, { onDelete: "cascade" }),
+  front: text().notNull(),
+  back: text().notNull(),
+  createdAt: timestamp().notNull().defaultNow(),
+  updatedAt: timestamp().notNull().defaultNow(),
+});
